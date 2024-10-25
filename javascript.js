936cc06991d956f6d732e4c907099042ffc07682
muratakdniz <muratakdeniz.145@gmail.com>
2024-10-25 10:04:42 +0300

    7 güncelle

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 function checkWidth() {
     const iconbars = document.querySelector('.iconbars');
-    if (window.innerWidth >= 320 && window.innerWidth <= 1025) { 
-        iconbars.style.display = 'block'; 
+    if (window.innerWidth >= 320 && window.innerWidth <= 1025) {
+        iconbars.style.display = 'block';
     } else {
-        iconbars.style.display = 'none'; 
+        iconbars.style.display = 'none';
     }
 }
 
@@ -12,14 +12,14 @@ const sidebar = document.querySelector('.sidebar');
 const closingButton = document.querySelector('.fa-xmark');
 
 bar.addEventListener('click', () => {
-    sidebar.classList.toggle('showSidebar'); 
+    sidebar.classList.toggle('showSidebar');
 });
 
 closingButton.addEventListener('click', () => {
-    sidebar.classList.remove('showSidebar'); 
+    sidebar.classList.remove('showSidebar');
 });
 
 window.onload = checkWidth;
-window.onresize = checkWidth;
+// window.onresize = checkWidth;
 
 
